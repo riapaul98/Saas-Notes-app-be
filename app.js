2250e3f0969c71908a72afb4a329a33084bd0cc9
riapaul98 <ria17041998@gmail.com>
2025-12-28 17:07:52 +0530

    cors added for specific client url

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // to build express app and apply middlewares
 
+import dotenv from "dotenv";
+dotenv.config(); // Load env vars first
+
 import express from "express";
 import cors from "cors";
 import route from "./routes/authRoutes.js";
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+const allowedOrigins = [process.env.CLIENT_URL, process.env.PROD_CLIENT_URL];
 
+// CORS must be before routes
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
 app.use("/api/auth", route);
 
 export default app;
--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 //entry point - to run the server and DB connection
 
-import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-//Load environment variables
-dotenv.config();
 
 // Connect to database
 connectDB();
 
-// Start the server 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
